48c5a3d40529757d22bf0f089ca2dcd27e50df9f
Adam Vašíček <adam.vasicek@seznam.cz>
2016-01-07 14:21:05 +0100

    fix search bug, locales links on search and completed translations

--- app/controllers/tolk/searches_controller.rb
+++ app/controllers/tolk/searches_controller.rb
@@ -1,7 +1,8 @@
 module Tolk
   class SearchesController < Tolk::ApplicationController
     before_filter :find_locale
-  
+    skip_load_and_authorize_resource
+
     def show
       @phrases = @locale.search_phrases(params[:q], params[:scope].to_sym, params[:k], params[pagination_param])
     end
