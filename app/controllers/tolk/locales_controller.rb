85d0d2b95df1f3c4274a5bdda91c1068e8bd429a
Adam Vašíček <adam.vasicek@seznam.cz>
2016-01-29 07:17:26 +0100

    refactor cancan resource authorization

--- app/controllers/tolk/locales_controller.rb
+++ app/controllers/tolk/locales_controller.rb
@@ -3,25 +3,16 @@ module Tolk
     before_filter :find_locale, :only => [:show, :all, :update, :updated]
     before_filter :find_source_language_name, :only => [:show]
     before_filter :ensure_no_primary_locale, :only => [:all, :update, :show, :updated]
-    skip_load_and_authorize_resource
+
+    skip_load_resource
     load_resource :find_by => :name
     authorize_resource
 
     def index
-      # hotfix
-      # todo: rewrite to use Ability instead of this redirect
-      redirect_to locale_path(@current_user.language.locale_shortcut) if @current_user.manager?
-
       @locales = Tolk::Locale.secondary_locales.sort_by(&:language_name)
     end
 
     def show
-      # hotfix
-      # todo: rewrite to use Ability instead of this redirect
-      if @current_user.manager? && @current_user.language.locale_shortcut != @locale.name
-        redirect_to locale_path(@current_user.language.locale_shortcut)
-      end
-
       respond_to do |format|
         format.html do
           @phrases = @locale.phrases_without_translation(params[pagination_param])
--- app/controllers/tolk/searches_controller.rb
+++ app/controllers/tolk/searches_controller.rb
@@ -1,9 +1,13 @@
 module Tolk
   class SearchesController < Tolk::ApplicationController
     before_filter :find_locale
+
     skip_load_and_authorize_resource
+    load_resource :locale, :parent => false, :find_by => :name , :class => 'Tolk::Locale'
 
     def show
+      authorize! :show, @locale
+
       @phrases = @locale.search_phrases(params[:q], params[:scope].to_sym, params[:k], params[pagination_param])
     end
 
